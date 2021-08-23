19b95ce88f1879663c9b458ba6775d823fe20366
ArShown <hles51723@gmail.com>
2021-08-23 08:21:26 +0800

    feat: 調整內容判斷邏輯

--- src/create-i18n.js
+++ src/create-i18n.js
@@ -30,21 +30,32 @@ function I18n(options) {
 
   this.addMessages = function addMessages(locale, msgs) {
     constants.messages[locale] = mergeDeepRight(
-      constants.messages[locale] || {},
+      constants.messages[locale] ?? {},
       msgs
     );
   };
  
   const regExp = new RegExp(/\{([^}]*)\}/, "g");
   this.t = function t(key, params = {}) {
-    const initMessages = constants.messages[currentLocale.value] || {};
-    const fallbackMessages = constants.messages[config.fallbackLocale] || {};
-    const value = initMessages[key] || fallbackMessages[key] || key;
+    const initMessages = constants.messages[currentLocale.value] ?? {};
+    const fallbackMessages = constants.messages[config.fallbackLocale] ?? {};
+
+    let value = key;
+
+    if(key in initMessages)
+      value = initMessages[key];
+    else if(key in fallbackMessages)
+      value = fallbackMessages[key];
+
     const matches = match(regExp, value);
     return reduce((str, match) => {
       const replaceRegExp = new RegExp(match);
       const key = match.substring(1, match.length - 1);
-      return params[key] ? replace(replaceRegExp, params[key], str) : str;
+
+      if(key in params)
+        return replace(replaceRegExp, params[key], str);
+        
+      return str;
     }, clone(value))(matches);
   };
 }
